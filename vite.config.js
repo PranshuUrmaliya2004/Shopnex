2fa77f235e80b4ccbab9c16544524dad023195fa
PranshuUrmaliya2004 <Pransuurmaliya2018@gmail.com>
2026-02-02 21:15:20 +0530

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,14 @@
+//  import { defineConfig } from 'vite'
+//  import react from '@vitejs/plugin-react'
+// import tailwindcss from '@tailwindcss/vite'
+//   https://vite.dev/config/
+// export default defineConfig({
+//  plugins: [react(),tailwindcss()],
+//   server  :{port:5173}
+  
+//    }
+// )
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
@@ -5,4 +16,7 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(),tailwindcss()],
+  server : {port:5173}
 })
+
+
